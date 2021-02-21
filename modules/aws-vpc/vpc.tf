d213c8d441f4b7565f12386918aa9d1b49367976
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-02-21 12:46:05 +0700

    Create vpc.tf

---
+++ modules/aws-vpc/vpc.tf
@@ -0,0 +1,53 @@
+locals {
+  domain_name       = var.domain_name == "" ? "${var.aws_region}.compute.internal" : var.domain_name
+  vpc_name          = "${var.tags["core.surname"]}-${var.app_name}-vpc"
+  dhcp_options_name = "${var.tags["core.surname"]}-${var.app_name}-dhcp-os"
+}
+
+resource "aws_vpc" "this" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = var.enable_private_dns
+  enable_dns_hostnames = var.enable_private_dns
+
+  tags = merge(
+    {
+      Name = local.vpc_name,
+    },
+    var.tags
+  )
+}
+
+resource "aws_vpc_ipv4_cidr_block_association" "this" {
+  count = length(var.additional_cidr_blocks)
+
+  cidr_block = var.additional_cidr_blocks[count.index]
+  vpc_id     = aws_vpc.this.id
+}
+
+resource "aws_vpc_dhcp_options" "this" {
+  domain_name         = local.domain_name
+  domain_name_servers = var.domain_name_servers
+
+  tags = merge(
+    {
+      Name = local.dhcp_options_name,
+    },
+    var.tags
+  )
+}
+
+resource "aws_vpc_dhcp_options_association" "this" {
+  dhcp_options_id = aws_vpc_dhcp_options.this.id
+  vpc_id          = aws_vpc.this.id
+}
+
+resource "aws_default_security_group" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(
+    {
+      Name = local.vpc_name,
+    },
+    var.tags
+  )
+}
