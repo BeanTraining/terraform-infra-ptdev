1068bac9b77086982ca1c1a4dbde420454ae394a
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-02-21 12:02:41 +0700

    Update variables.tf

--- environments/master/apps/eks/controlplane/variables.tf
+++ environments/master/apps/eks/controlplane/variables.tf
@@ -41,30 +41,6 @@ variable "secondary_aws_account_ids" {
   type = map(string)
 }
 
-variable "env_vpn_cidr_blocks" {
-  description = "CIDRs from VPN VPC/Subnets - communication between AWS accounts"
-
-  type = list(string)
-}
-
-variable "spinnaker_cidr_blocks" {
-  description = "Spinnaker CIDRs from crossenv - enable access to EKS/K8S API"
-
-  type = list(string)
-}
-
-variable "vault_cidr_blocks" {
-  description = "Vault CIDRs from shared - enable access from vault to eks "
-
-  type = list(string)
-}
-
-variable "tfc_cidr_blocks" {
-  description = "tfc CIDRs from crossenv - enable access from tfc to eks "
-
-  type = list(string)
-}
-
 variable "owner" {
   description = "Name of the team responsible for these resources"
 
