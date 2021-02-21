4e010af0f0032f385946723643ffd2ca58e9bc45
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-02-21 15:06:10 +0700

    Create vpc.tf

---
+++ skeleton/apps/vpc/vpc.tf
@@ -0,0 +1,19 @@
+locals {
+  tags = merge(
+    module.mox_tags.vpc_mandatory,
+    var.additional_tags,
+  )
+}
+
+module "vpc" {
+  source = "../../../modules/aws-vpc"
+
+  app_name               = var.app_name
+  cidr_block             = var.cidr_block
+  additional_cidr_blocks = var.additional_cidr_blocks
+  environment            = var.environment
+  enable_private_dns     = var.enable_private_dns
+  aws_region             = var.aws_region
+  aws_account_id         = var.aws_account_id
+  tags                   = local.tags
+}
