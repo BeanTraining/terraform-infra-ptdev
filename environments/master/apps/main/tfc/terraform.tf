bdef5da9feb1ab4d944f06a7d7d9bc5db11f732b
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-02-22 18:56:14 +0700

    Update terraform.tf

--- environments/master/apps/main/tfc/terraform.tf
+++ environments/master/apps/main/tfc/terraform.tf
@@ -8,8 +8,8 @@ resource "tfe_agent_pool" "bean-agent-pool" {
   organization = tfe_organization.bean-training.id
 }
 
-resource "tfe_workspace" "sg-dev-main-apps-vpc " {
-  name           = "sg_dev_main_apps_vpc "
+resource "tfe_workspace" "sg-dev-main-apps-vpc" {
+  name           = "sg-dev-main-apps-vpc"
   organization   = tfe_organization.bean-training.id
   agent_pool_id  = tfe_organization.bean-agent-pool.id
   execution_mode = "agent"
