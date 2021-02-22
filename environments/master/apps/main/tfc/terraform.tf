04bde3c1567d3f9413c54d84184ad0aebb2933a2
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-02-22 19:46:25 +0700

    Update terraform.tf

--- environments/master/apps/main/tfc/terraform.tf
+++ environments/master/apps/main/tfc/terraform.tf
@@ -7,6 +7,6 @@ resource "tfe_variable" "test" {
   key          = "my_key_name"
   value        = "my_value_name"
   category     = "terraform"
-  workspace_id = tfe_workspace.sg-dev-main-apps-vpc.id
+  workspace_id = data.tfe_workspace.sg-dev-main-apps-vpc.id
   description  = "a useful description"
 }
