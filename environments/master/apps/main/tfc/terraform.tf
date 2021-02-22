72a6a19e5ca53d21d28ec062f73abde117c0fd5c
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-02-22 23:46:18 +0700

    Update terraform.tf

--- environments/master/apps/main/tfc/terraform.tf
+++ environments/master/apps/main/tfc/terraform.tf
@@ -7,10 +7,15 @@ provider "tfe" {
   hostname = "app.terraform.io" # Terraform Cloud
 }
 
-resource "tfe_variable" "test" {
+resource "tfe_variable" "example" {
   key          = "bean_site"
   value        = "bean.academy"
   category     = "terraform"
   workspace_id = data.tfe_workspace.sg-dev-main-apps-example.id
   description  = "a useful description"
+}
+
+data "tfe_workspace" "sg-dev-main-apps-vpc" {
+  name           = "sg-dev-main-apps-vpc"
+  organization   = "BeanTraining"
 }
