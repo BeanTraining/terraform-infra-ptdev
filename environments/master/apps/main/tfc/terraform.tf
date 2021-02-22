36430ee886310950aaf6462d94fe6719d335e00b
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-02-22 19:43:58 +0700

    Update terraform.tf

--- environments/master/apps/main/tfc/terraform.tf
+++ environments/master/apps/main/tfc/terraform.tf
@@ -2,3 +2,11 @@ data "tfe_workspace" "sg-dev-main-apps-vpc" {
   name           = "sg-dev-main-apps-vpc"
   organization   = "BeanTraining"
 }
+
+resource "tfe_variable" "test" {
+  key          = "my_key_name"
+  value        = "my_value_name"
+  category     = "terraform"
+  workspace_id = tfe_workspace.sg-dev-main-apps-vpc.id
+  description  = "a useful description"
+}
