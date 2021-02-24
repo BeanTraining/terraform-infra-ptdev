dd70aef57e290f2168aea58e679729e0a8da3f33
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-02-24 17:13:36 +0700

    Delete skeleton/apps/eks/controlplane directory

--- skeleton/apps/eks/controlplane/local.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  cluster_name = "${var.environment}-${var.application}"
-
-  eks_ingress_443_cidrs = concat(
-    var.eks_ingress_443_cidrs,
-    var.env_vpn_subnet_cidrs,
-    var.env_vpn_client_cidrs,
-  )
-}
