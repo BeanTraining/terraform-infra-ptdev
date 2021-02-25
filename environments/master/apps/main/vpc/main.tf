fe5749bbf2ffceae5e1e91d45d002a566864332d
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-02-25 17:28:34 +0700

    Update main.tf

--- environments/master/apps/main/vpc/main.tf
+++ environments/master/apps/main/vpc/main.tf
@@ -1,7 +1,6 @@
 module "skeleton" {
   source                 = "../../../../../skeleton/apps/vpc"
   app_name               = var.app_name
-  environment            = var.environment
   cidr_block             = var.cidr_block
   bounded_context        = var.bounded_context
 }
