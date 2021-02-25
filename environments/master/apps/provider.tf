bb5b255b1252d97b0d245f2ea88f8e0144736800
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-02-25 18:25:51 +0700

    Update provider.tf

--- environments/master/apps/provider.tf
+++ environments/master/apps/provider.tf
@@ -3,10 +3,5 @@ terraform {
 }
 
 provider "aws" {
-  region  = var.secondary_aws_region
   version = "3.12.0"
-
-  assume_role {
-    role_arn = "arn:aws:iam::${var.secondary_aws_account_ids.apps}:role/TerraformBaselineRole"
-  }
 }
