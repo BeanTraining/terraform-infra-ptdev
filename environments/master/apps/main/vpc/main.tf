41ffb452f8ee124d45558a429e9c89d8a55cf631
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-02-25 16:40:49 +0700

    Update main.tf

--- environments/master/apps/main/vpc/main.tf
+++ environments/master/apps/main/vpc/main.tf
@@ -4,4 +4,5 @@ module "skeleton" {
   source                 = "../../../../../skeleton/apps/vpc"
   app_name               = var.app_name
   environment            = var.environment
+  cidr_block             = var.cidr_block
 }
