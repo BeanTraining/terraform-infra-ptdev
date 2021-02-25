85575f852026fd11274e6693bd23d29b730a1d34
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-02-25 16:40:10 +0700

    Update variables.tf

--- environments/master/apps/main/vpc/variables.tf
+++ environments/master/apps/main/vpc/variables.tf
@@ -1,12 +1,4 @@
-variable "additional_vpc_cidr_blocks" {
-  type = list(string)
-}
-
 variable "app_name" {
-  type = string
-}
-
-variable "secondary_aws_region" {
   type = string
 }
 
@@ -14,58 +6,6 @@ variable "environment" {
   type = string
 }
 
-variable "secondary_aws_account_ids" {
-  description = "List of aws accounts ids for environment"
-
-  type = map
-}
-
-variable "secondary_aws_account_names" {
-  description = "List of aws account names for environment"
-
-  type = map
-}
-
-variable "vpc_cidr_block" {
-  type = string
-}
-
-variable "vpc_enable_private_dns" {
-  type = bool
-}
-
-variable "environment_short" {
-  description = "Environment name without region suffix"
-
-  type = string
-}
-
-variable "bean_site" {
-  description = "Bean site - primary or secondary"
-
-  type = string
-}
-
-variable "bounded_context" {
-  description = "Bounded context"
-
-  type = string
-}
-
-variable "data_class_names" {
-  description = "Data Class tag possible names"
-
-  type = map(string)
-}
-
-variable "tf_tags" {
-  description = "Map of tags to identify terraform code base and workspace"
-
-  type = map(string)
-}
-
-variable "owner" {
-  description = "Bean Owner value for Core Foundation resources"
-
+variable "cidr_block" {
   type = string
 }
