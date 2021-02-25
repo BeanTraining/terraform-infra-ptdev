ddb5c62c7141a7466cde3a4b029d167b55f5a987
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-02-25 17:25:01 +0700

    Update vpc.tf

--- skeleton/apps/vpc/vpc.tf
+++ skeleton/apps/vpc/vpc.tf
@@ -1,6 +1,5 @@
 module "vpc" {
   source = "../../../modules/aws-vpc"
   app_name               = var.app_name
-  environment            = var.environment
   cidr_block             = var.cidr_block
 }
