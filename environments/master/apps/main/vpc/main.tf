ea41a34918dd2fa0f5abb0ecc13fd634c1302345
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-02-25 17:17:41 +0700

    Update main.tf

--- environments/master/apps/main/vpc/main.tf
+++ environments/master/apps/main/vpc/main.tf
@@ -1,8 +1,7 @@
-data "aws_caller_identity" "current" {}
-
 module "skeleton" {
   source                 = "../../../../../skeleton/apps/vpc"
   app_name               = var.app_name
   environment            = var.environment
   cidr_block             = var.cidr_block
+  bounded_context        = var.bounded_context
 }
