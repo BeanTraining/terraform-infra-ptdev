31d5b5b200aa869b00b5bcdfec6b6cc5c6fdcf7d
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-03-02 02:15:12 +0700

    Update tfe_workspaces.tf

--- environments/master/apps/main/tfc/tfe_workspaces.tf
+++ environments/master/apps/main/tfc/tfe_workspaces.tf
@@ -27,11 +27,15 @@ variable "workspaces" {
       app_type = "apps"
       app_category = "main"
       app_name = "vpc"
-      environment = "dev-sg"
       trigger_prefixes = []
       }
      ]
 }
+
+variable "environment" {
+   type = string
+   default = "dev-sg"
+   }
 
 resource "tfe_oauth_client" "bean-github" {
   organization     = "BeanTraining"
@@ -42,23 +46,23 @@ resource "tfe_oauth_client" "bean-github" {
 }
 
 resource "tfe_workspace" "bean" {
-  for_each = {for ws in var.workspaces:  "${ws.environment}-${ws.app_type}-${ws.app_category}-${ws.app_name}" => ws}
-  name                = "${each.value.environment}-${each.value.app_type}-${each.value.app_category}-${each.value.app_name}"
+  for_each = {for ws in var.workspaces:  "${var.environment}-${ws.app_type}-${ws.app_category}-${ws.app_name}" => ws}
+  name                = "${var.environment}-${each.value.app_type}-${each.value.app_category}-${each.value.app_name}"
   organization        = "BeanTraining"
   speculative_enabled = false
   working_directory   = "${each.value.base_directory}-${each.value.app_type}-${each.value.app_category}-${each.value.app_name}"
   trigger_prefixes    = concat(each.value.trigger_prefixes,
      [
-     each.value.working_directory,
-     "/environments/${each.value.environment}/apps/main/tfc/releases"
+        "${each.value.base_directory}-${each.value.app_type}-${each.value.app_category}-${each.value.app_name}",
+        "/environments/${var.environment}/apps/main/tfc/releases"
      ]
      )
   vcs_repo {
     identifier       = "BeanTraining/terraform-infra"
-    branch           = each.value.environment
+    branch           = var.environment
     oauth_token_id   = tfe_oauth_client.bean-github.oauth_token_id
     }
-  auto_apply         = each.value.environment == "master" ? false : true
+  auto_apply         = var.environment == "master" ? false : true
 }
 
 
