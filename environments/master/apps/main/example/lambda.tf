5345e95d9dd70e74dd8e85f7bf83135e9afaa222
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-03-04 20:30:36 +0700

    Update lambda.tf

--- environments/master/apps/main/example/lambda.tf
+++ environments/master/apps/main/example/lambda.tf
@@ -88,7 +88,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 resource "aws_iam_role_policy_attachment" "AWSLambdaVPCAccessExecutionRole" {
-    role       = aws_iam_role.lambda.name
+    role       = aws_iam_role.iam_for_lambda.name
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
