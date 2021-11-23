228ab41464eb91c812655409fcf70bdcd38de78f
EC2 Default User <ec2-user@ip-172-31-35-75.ap-southeast-1.compute.internal>
2021-11-23 08:26:44 +0000

    Test NAT

--- platforms/simple-oregon/apps/vpc/kops/temp.tf
+++ platforms/simple-oregon/apps/vpc/kops/temp.tf
@@ -3,7 +3,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["KopsBastionUbuntu20Focal*"]
   }
 
   filter {
@@ -11,7 +11,7 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  owners = ["099720109477"] # Canonical
+  owners = ["249617153445"] # SharedBean ???
 }
 
 data "aws_ami" "amazon2" {
