cec4d10ece4411bf47050bc473ad10ec31438851
EC2 Default User <ec2-user@ip-172-31-35-75.ap-southeast-1.compute.internal>
2021-11-24 16:08:05 +0000

    New Temp

--- platforms/simple-oregon/apps/vpc/kops/nat_instance_eni_first.tf
+++ platforms/simple-oregon/apps/vpc/kops/nat_instance_eni_first.tf
@@ -72,25 +72,52 @@ resource "aws_instance" "private" {
   }
 }
 
-# resource "aws_instance" "nat" {
-#   source_dest_check = false
-#   ami           = data.aws_ami.nat_ami_amazon2.id
-#   instance_type = "t4g.micro"
-#   subnet_id     = module.skeleton.vpc_public_subnet_ids[0]
-#   vpc_security_group_ids = [aws_security_group.allow_all.id]
-  
-#   key_name      = "shared_deployer_peterbean"
+resource "aws_instance" "nat" {
+  source_dest_check = false
+  ami           = data.aws_ami.nat_ami_amazon2.id
+  instance_type = "t4g.micro"
 
-#   tags = {
-#     Name = "NAT managed by tf"
+  network_interface {
+    network_interface_id = aws_network_interface.nat.id
+    device_index         = 0
+  }
+
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+  
+  key_name      = "shared_deployer_peterbean"
+
+  tags = {
+    Name = "NAT managed by tf"
+  }
+}
+
+# resource "aws_instance" "foo" {
+#   ami           = "ami-005e54dee72cc1d00" # us-west-2
+#   instance_type = "t2.micro"
+
+#   network_interface {
+#     network_interface_id = aws_network_interface.foo.id
+#     device_index         = 0
+#   }
+
+#   credit_specification {
+#     cpu_credits = "unlimited"
 #   }
 # }
+
 
 # resource "aws_route" "nat" {
 #   route_table_id              = module.skeleton.vpc_private_route_table_ids[0]
 #   destination_cidr_block    = "0.0.0.0/0"
 #   network_interface_id      = aws_instance.nat.primary_network_interface_id
 # }
+
+
+resource "aws_network_interface" "nat" {
+  subnet_id       = module.skeleton.vpc_public_subnet_ids[0]
+  private_ips     = ["10.0.4.1"]
+  security_groups = [aws_security_group.allow_all.id]
+}
 
 resource "aws_security_group" "allow_all" {
   name        = "allow_all"
