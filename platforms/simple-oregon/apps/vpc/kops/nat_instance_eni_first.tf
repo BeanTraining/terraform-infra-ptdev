45419eb2b55e5757ab27c8e753d2c05465795287
EC2 Default User <ec2-user@ip-172-31-35-75.ap-southeast-1.compute.internal>
2021-11-24 18:38:00 +0000

    ENI first

--- platforms/simple-oregon/apps/vpc/kops/nat_instance_eni_first.tf
+++ platforms/simple-oregon/apps/vpc/kops/nat_instance_eni_first.tf
@@ -88,27 +88,11 @@ resource "aws_instance" "nat" {
   }
 }
 
-# resource "aws_instance" "foo" {
-#   ami           = "ami-005e54dee72cc1d00" # us-west-2
-#   instance_type = "t2.micro"
-
-#   network_interface {
-#     network_interface_id = aws_network_interface.foo.id
-#     device_index         = 0
-#   }
-
-#   credit_specification {
-#     cpu_credits = "unlimited"
-#   }
-# }
-
-
-# resource "aws_route" "nat" {
-#   route_table_id              = module.skeleton.vpc_private_route_table_ids[0]
-#   destination_cidr_block    = "0.0.0.0/0"
-#   network_interface_id      = aws_instance.nat.primary_network_interface_id
-# }
-
+resource "aws_route" "nat" {
+  route_table_id              = module.skeleton.vpc_private_route_table_ids[0]
+  destination_cidr_block    = "0.0.0.0/0"
+  network_interface_id      = aws_network_interface.nat.id
+}
 
 resource "aws_network_interface" "nat" {
   subnet_id       = module.skeleton.vpc_public_subnet_ids[0]
