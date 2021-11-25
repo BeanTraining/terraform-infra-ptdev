f09222a5239b95d50b806914ef0150410b8b5fcd
EC2 Default User <ec2-user@ip-172-31-35-75.ap-southeast-1.compute.internal>
2021-11-25 07:31:03 +0000

    ASG

--- platforms/simple-oregon/apps/vpc/kops/nat_instance_asg.tf
+++ platforms/simple-oregon/apps/vpc/kops/nat_instance_asg.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "bastion" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
   subnet_id              = module.skeleton.vpc_public_subnet_ids[0]
-  # vpc_security_group_ids = [aws_security_group.allow_all.id]
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
 
   key_name = "shared_deployer_peterbean"
 
@@ -48,7 +48,7 @@ resource "aws_instance" "private" {
   ami                    = data.aws_ami.amazon2.id
   instance_type          = "t4g.micro"
   subnet_id              = module.skeleton.vpc_private_subnet_ids[0]
-  # vpc_security_group_ids = [aws_security_group.allow_all.id]
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
 
   key_name = "shared_deployer_peterbean"
 
@@ -57,3 +57,31 @@ resource "aws_instance" "private" {
   }
 }
 
+
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow ALL inbound traffic"
+  vpc_id      = module.skeleton.vpc_id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
