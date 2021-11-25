f926bf8edd9eec6f0c998478ef892c2351dfbb5c
EC2 Default User <ec2-user@ip-172-31-35-75.ap-southeast-1.compute.internal>
2021-11-25 06:03:39 +0000

    ASG

--- platforms/simple-oregon/apps/vpc/kops/main.tf
+++ platforms/simple-oregon/apps/vpc/kops/main.tf
@@ -1,6 +1,6 @@
 module "skeleton" {
-  source = "git::ssh://git@github.com/BeanTraining/terraform-infra-skeleton.git//apps/vpc?ref=ptdev"
-  vpc_name        = "${var.app_name}-${var.app_category}-${var.app_type}-${var.platform}-${var.environment}"
+  source   = "git::ssh://git@github.com/BeanTraining/terraform-infra-skeleton.git//apps/vpc?ref=ptdev"
+  vpc_name = "${var.app_name}-${var.app_category}-${var.app_type}-${var.platform}-${var.environment}"
   # cidr_block      = var.cidr_block
   bounded_context = "network"
 }
--- platforms/simple-oregon/apps/vpc/kops/nat_instance_asg.tf
+++ platforms/simple-oregon/apps/vpc/kops/nat_instance_asg.tf
@@ -47,12 +47,12 @@ data "aws_ami" "nat_ami_amazon2" {
 }
 
 resource "aws_instance" "bastion" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  subnet_id     = module.skeleton.vpc_public_subnet_ids[0]
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  subnet_id              = module.skeleton.vpc_public_subnet_ids[0]
   vpc_security_group_ids = [aws_security_group.allow_all.id]
-  
-  key_name      = "shared_deployer_peterbean"
+
+  key_name = "shared_deployer_peterbean"
 
   tags = {
     Name = "Bastion"
@@ -60,13 +60,13 @@ resource "aws_instance" "bastion" {
 }
 
 resource "aws_instance" "private" {
-  ami           = 
-  instance_type = "t4g.micro"
-  subnet_id     = module.skeleton.vpc_private_subnet_ids[0]
+  ami                    = data.aws_ami.amazon2.id
+  instance_type          = "t4g.micro"
+  subnet_id              = module.skeleton.vpc_private_subnet_ids[0]
   vpc_security_group_ids = [aws_security_group.allow_all.id]
 
-  key_name      = "shared_deployer_peterbean"
-  
+  key_name = "shared_deployer_peterbean"
+
   tags = {
     Name = "PrivateInstance"
   }
@@ -74,7 +74,7 @@ resource "aws_instance" "private" {
 
 resource "aws_launch_template" "nat" {
   name_prefix   = "nat"
-  image_id      = data.aws_ami.amazon2.id
+  image_id      = data.aws_ami.nat_ami_amazon2.id
   instance_type = "t4g.micro"
   network_interfaces {
     network_interface_id = aws_network_interface.nat.id
@@ -95,17 +95,17 @@ resource "aws_autoscaling_group" "nat" {
 }
 
 resource "aws_route" "nat" {
-  route_table_id              = module.skeleton.vpc_private_route_table_ids[0]
-  destination_cidr_block    = "0.0.0.0/0"
-  network_interface_id      = aws_network_interface.nat.id
+  route_table_id         = module.skeleton.vpc_private_route_table_ids[0]
+  destination_cidr_block = "0.0.0.0/0"
+  network_interface_id   = aws_network_interface.nat.id
 }
 
 resource "aws_network_interface" "nat" {
-  subnet_id       = module.skeleton.vpc_public_subnet_ids[0]
-  private_ips     = ["10.0.4.10"]
-  security_groups = [aws_security_group.allow_all.id]
+  subnet_id         = module.skeleton.vpc_public_subnet_ids[0]
+  private_ips       = ["10.0.4.10"]
+  security_groups   = [aws_security_group.allow_all.id]
   source_dest_check = false
-  
+
 }
 
 resource "aws_security_group" "allow_all" {
