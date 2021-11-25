ae6f0d177abfdf4933fc3a80aa2ae07767daf215
EC2 Default User <ec2-user@ip-172-31-35-75.ap-southeast-1.compute.internal>
2021-11-25 08:02:43 +0000

    ASG

--- platforms/simple-oregon/apps/vpc/kops/main.tf
+++ platforms/simple-oregon/apps/vpc/kops/main.tf
@@ -3,4 +3,5 @@ module "skeleton" {
   vpc_name = "${var.app_name}-${var.app_category}-${var.app_type}-${var.platform}-${var.environment}"
   # cidr_block      = var.cidr_block
   bounded_context = "network"
+  nat_instance_sg_id = aws_security_group.allow_all.id
 }
