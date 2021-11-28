e9eabae97168024b7e8d8f8c1be66ba57a05ad39
EC2 Default User <ec2-user@ip-172-31-35-75.ap-southeast-1.compute.internal>
2021-11-28 02:32:49 +0000

    crossenv ws

---
+++ platforms/simple-oregon/apps/vpc/crossenv/nat_instance_asg.tf
@@ -0,0 +1,85 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["KopsBastionUbuntu20Focal*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["249617153445"] # SharedBean ???
+}
+
+data "aws_ami" "amazon2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20211103.1-arm64-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"] # Canonical
+}
+
+
+resource "aws_instance" "bastion" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  subnet_id              = module.skeleton.vpc_public_subnet_ids[0]
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  key_name = "shared_deployer_peterbean"
+
+  tags = {
+    Name = "Bastion"
+  }
+}
+
+resource "aws_instance" "private" {
+  ami                    = data.aws_ami.amazon2.id
+  instance_type          = "t4g.micro"
+  subnet_id              = module.skeleton.vpc_private_subnet_ids[0]
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  key_name = "shared_deployer_peterbean"
+
+  tags = {
+    Name = "PrivateInstance"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow ALL inbound traffic"
+  vpc_id      = module.skeleton.vpc_id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
---
+++ platforms/simple-oregon/apps/vpc/crossenv/terraform.auto.tfvars
@@ -0,0 +1,8 @@
+cidr_block      = "172.32.0.0/16"
+public_subnet_cidr_blocks = ["172.32.64.0/24", "172.32.65.0/24", "172.32.66.0/24"]
+private_subnet_cidr_blocks = ["172.32.0.0/20","172.32.16.0/20","172.32.32.0/20"]
+bounded_context = "network"
+app_name        = "crossenv"
+app_category    = "vpc"
+app_type        = "apps"
+
