4338e57ab829779c5aaa9cf59a92e6a2876e517a
EC2 Default User <ec2-user@ip-172-31-35-75.ap-southeast-1.compute.internal>
2021-11-28 02:39:08 +0000

    Love you

--- platforms/simple-oregon/apps/vpc/crossenv/main.tf
+++ platforms/simple-oregon/apps/vpc/crossenv/main.tf
@@ -9,3 +9,30 @@ module "skeleton" {
   public_subnet_cidr_blocks = var.public_subnet_cidr_blocks
   private_subnet_cidr_blocks = var.private_subnet_cidr_blocks
 }
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow ALL inbound traffic"
+  vpc_id      = module.skeleton.vpc_id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
--- platforms/simple-oregon/apps/vpc/kops/main.tf
+++ platforms/simple-oregon/apps/vpc/kops/main.tf
@@ -6,3 +6,30 @@ module "skeleton" {
   nat_instance_sg_id = aws_security_group.allow_all.id
   aws_region = var.aws_region
 }
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow ALL inbound traffic"
+  vpc_id      = module.skeleton.vpc_id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
